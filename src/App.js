dfeaabc40578dd6e3a0132f08628ebfbdbf55ddd
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-08-13 02:06:22 +0530

    Finalised version of multistep form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
-
-import "./App.css";
-import Form from "./components/Form";
+import "./styles/App.css";
+import StepOne from "./form/StepOne";
+import StepTwo from "./form/StepTwo";
+import StepThree from "./form/StepThree";
+import Head from "./components/Head";
+import { useSelector } from "react-redux";
 
 function App() {
+  const data = useSelector((state) => state);
+
   return (
     <div className="app">
-      <Form />
+      <div className="app-child">
+        {!data.stepOne || (data.active === 1 && data.stepOne) ? (
+          <>
+            <Head />
+            <StepOne />
+          </>
+        ) : !data.stepTwo || (data.active === 2 && data.stepTwo) ? (
+          <>
+            <Head />
+            <StepTwo />
+          </>
+        ) : !data.stepThree ? (
+          <>
+            <Head />
+            <StepThree />
+          </>
+        ) : (
+          <h3>Submitted successfully!{console.log(data)}</h3>
+        )}
+      </div>
     </div>
   );
 }
